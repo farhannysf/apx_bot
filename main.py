86b475b461e5e8d9ac0973acffe17ce8fd1d5754
Farhan Yusuf Nugroho <farhannyusuf@yahoo.co.uk>
2019-02-17 11:05:24 -0800

    Update server list

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix='!')
 
-serverList = {1:'1015436', 4:'1925117'}
+serverList = {1:'3219782', 4:'3244423'}
 
 async def asyncGet(*args, **kwargs):
     async with aiohttp.ClientSession() as client:
@@ -40,15 +40,15 @@ async def embify(serverData, playerData):
     embed = discord.Embed(title=serverName, description=serverStats.title(), color=0x00ff00)
     embed.set_thumbnail(url='https://units.arma3.com/groups/img/32641/06EvpC7yf0.png')
     
-    if serverStats == 'offline':
-        return embed
-    
-    elif serverStats == 'online':
+    if serverStats == 'online':
         embed.add_field(name="Mission", value=serverMission, inline=True)
         embed.add_field(name="Players", value=f'{activePlayer}/{maxPlayer}', inline=True)
         embed.add_field(name="Active Players", value=players, inline=False)
-        
-        return embed
+ 
+    else:
+        return embed  
+    
+    return embed
 
 
 @client.event
@@ -72,6 +72,8 @@ async def serverstats(ctx, serverNumber:int=None):
                 playerData = await getData('https://api.battlemetrics.com/players', params={'filter[servers]':server, 
                     'filter[online]':'true', 'page[size]':serverData['data']['attributes']['maxPlayers']})
                 embed = await embify(serverData, playerData)
+                print (serverData)
+                print (embed)
                 await client.say(embed=embed)
         
         else:
