d6666199cf78f0e106ef2a4ef10908ec7ba381f7
Farhan Yusuf Nugroho <farhannyusuf@yahoo.co.uk>
2019-02-19 23:22:57 -0800

    Delete previous API keys configuration

--- settings.py
+++ settings.py
@@ -1,16 +1,10 @@
 from os import environ
 from dotenv import load_dotenv, find_dotenv
 
-def retrieveKeys(db):
+def retrievegit Keys(db):
     users_ref = db.collection(u'keys')
     docs = users_ref.get()
     for doc in docs:
         return doc.to_dict()   
 
 load_dotenv(find_dotenv())
-
-# discordToken = environ['discordToken']
-# channelId = environ['channelId']
-# authorId = environ['authorId']
-# sentryUrl = environ['sentryUrl']
-# segmentKey = environ['segmentKey']
