aeedb2f7aa779f101d7c84e32f8b4851d133596c
Farhan Yusuf Nugroho <farhannyusuf@yahoo.co.uk>
2019-03-14 10:38:58 -0700

    Add server list management command

--- main.py
+++ main.py
@@ -1,30 +1,20 @@
 import asyncio
-import aiohttp
 import discord
 import settings
-import discord
 import logging
 import sentry_sdk
 import analytics
 
-from sanic import Sanic
-from google.cloud import firestore
 from discord.ext import commands
+from google.cloud import firestore
 from sentry_sdk import capture_message
 
-async def webserver():
-    app = Sanic(__name__)
-
-    @app.route("/overridechat", methods=["POST"])
-    async def chat_override(request):
-        return response.json({"status": "success"})
-
-    return await app.create_server(host="0.0.0.0", port=8080)
+client = commands.Bot(command_prefix='!')
 
 # Initiate Google Cloud Firestore
 db = firestore.Client()
-keys = settings.retrieveKeys(db)
-
+keys = settings.retrieveData(db, option='keys', title='api')
+ 
 # Initiate logging and sentry
 logging.basicConfig(level=logging.INFO)
 sentry_sdk.init(keys['sentryUrl'])
@@ -32,82 +22,62 @@ sentry_sdk.init(keys['sentryUrl'])
 # Initiate Segment analytics
 analytics.write_key = keys['segmentKey']
 
-client = commands.Bot(command_prefix='!')
+async def serverstatsLogic(ctx, serverNumber):
+    if ctx.message.channel.id == keys['channelId']:
+        serverList = settings.retrieveData(db, option='serverlist', title=ctx.message.channel.id)
+        if serverList is None:
+            return await client.say('No server is set. Use `!serverconfig` for more info.')
 
-serverList = {1:'3219782', 4:'3244423'}
+        try:
+            server = serverList[str(serverNumber)]
+        except:
+            availableServers = '\n'.join('Server {} (Battlemetrics ID: {})'.format(key, value) for key, value in serverList.items())
+            return await client.say(f'`Usage: !serverstats [server number]`\n\nAvailable servers:\n` {availableServers} `')
 
-async def asyncGet(*args, **kwargs):
-    async with aiohttp.ClientSession() as client:
-        async with client.get(*args, **kwargs) as response:
-            response.body = await response.read()
+        serverData = await settings.getData(f'https://api.battlemetrics.com/servers/{server}', params=None)
+            
+        if serverData is None:
+            return await client.say(f'An error has occured, please contact {author}')
+        
+        else:
+            playerData = await settings.getData('https://api.battlemetrics.com/players', params={'filter[servers]':server, 
+                'filter[online]':'true', 'page[size]':serverData['data']['attributes']['maxPlayers']})
+            embed = await settings.embify(serverData, playerData, discord.Embed)
+            return await client.say(embed=embed)
 
-            return response
+async def server_updateLogic(ctx, operation, serverNumber, serverId):
+    serverlistDb = db.collection('serverlist').document(str(ctx.message.channel.id))
+    await settings.checkDb(db, serverlistDb, ctx, firestore)
+    usageMessage = 'Usage:\n\n`!serverconfig update [server number] [battlmetrics server id]\n!serverconfig delete [server number]`'
 
-async def getData(url, params):
-    response = await asyncGet(url, params=params)
+    if operation == 'delete':
+        if serverNumber is None: 
+            return await client.say(usageMessage)
+        data = {str(serverNumber): firestore.DELETE_FIELD}
+        serverlistDb.update(data)
+        return await client.say(f'**Updated server list.**\n `Deleted Server {serverNumber}`')
+
+    if operation == 'update':
+        if serverNumber is None or serverId is None:
+            return await client.say(usageMessage)
+        data = {str(serverNumber):str(serverId)}
+        serverlistDb.update(data)
+        return await client.say(f'**Updated server list.**\n `Server {serverNumber} (Battlemetrics ID: {serverId})`')
     
-    if response.status == 200:
-        data = await response.json()
-        return data    
     else:
-        capture_message(f'Battlemetrics API: {response.status}')
-    
-async def embify(serverData, playerData):
-    if not playerData['data']:
-        players = 'No active players.'
-    
-    else:
-        playerList = [item['attributes']['name'] for item in playerData['data']]
-        players = '\n'.join(playerList)
-
-    serverName = serverData['data']['attributes']['name']
-    serverStats = serverData['data']['attributes']['status']
-    serverMission = serverData['data']['attributes']['details']['mission']
-    activePlayer = serverData['data']['attributes']['players']
-    maxPlayer = serverData['data']['attributes']['maxPlayers']
-    
-    embed = discord.Embed(title=serverName, description=serverStats.title(), color=0x00ff00)
-    embed.set_thumbnail(url='https://units.arma3.com/groups/img/32641/06EvpC7yf0.png')
-    
-    if serverStats == 'online':
-        embed.add_field(name="Mission", value=serverMission, inline=True)
-        embed.add_field(name="Players", value=f'{activePlayer}/{maxPlayer}', inline=True)
-        embed.add_field(name="Active Players", value=players, inline=False)
-    
-    elif serverStats == 'dead' or serverStats == 'removed':
-        capture_message(f'{serverName} is {serverStats}')
-
-    return embed
-
-
+        return await client.say(usageMessage)
+        
 @client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
-    client.loop.create_task(webserver())
-
 
 @client.command(pass_context=True)
 async def serverstats(ctx, serverNumber:int=None):
-    if ctx.message.channel.id == keys['channelId']:
-        if serverNumber == 1 or serverNumber == 4:
-            server = serverList[serverNumber]
-            serverData = await getData(f'https://api.battlemetrics.com/servers/{server}', params=None)
-            
-            if serverData is None:
-                await client.say(f'An error has occured, please contact {author}')
-            
-            else:
-                playerData = await getData('https://api.battlemetrics.com/players', params={'filter[servers]':server, 
-                    'filter[online]':'true', 'page[size]':serverData['data']['attributes']['maxPlayers']})
-                embed = await embify(serverData, playerData)
-                await client.say(embed=embed)
-        
-        else:
-            await client.say('`Usage: !serverstats [server number] (only server 1 and 4 are supported)`')
-    
+    await serverstatsLogic(ctx, serverNumber)
+
     analytics.track(ctx.message.author.id, 'Server Info Request', {
         'User ID': ctx.message.author.id,
         'Username': ctx.message.author.name,
@@ -118,6 +88,12 @@ async def serverstats(ctx, serverNumber:int=None):
         'Server number': serverNumber
     })
 
+@client.command(pass_context=True)
+@commands.has_permissions(manage_messages=True)
+async def serverconfig(ctx, operation:str=None, serverNumber:int=None, serverId:int=None):
+    await server_updateLogic(ctx, operation, serverNumber, serverId)
+
 if __name__ == '__main__':
     author = keys['authorId']
     client.run(keys['discordToken'])
+
--- settings.py
+++ settings.py
@@ -1,10 +1,62 @@
+import aiohttp
+
 from os import environ
 from dotenv import load_dotenv, find_dotenv
 
-def retrieveKeys(db):
-    users_ref = db.collection(u'keys')
-    docs = users_ref.get()
-    for doc in docs:
-        return doc.to_dict()   
+async def asyncGet(*args, **kwargs):
+    async with aiohttp.ClientSession() as client:
+        async with client.get(*args, **kwargs) as response:
+            response.body = await response.read()
+
+            return response
+
+async def getData(url, params):
+    response = await asyncGet(url, params=params)
+    
+    if response.status == 200:
+        data = await response.json()
+        return data    
+    else:
+        capture_message(f'Battlemetrics API: {response.status}')
+    
+async def embify(serverData, playerData, discordEmbed):
+    if not playerData['data']:
+        players = 'No active players.'
+    
+    else:
+        playerList = [item['attributes']['name'] for item in playerData['data']]
+        players = '\n'.join(playerList)
+
+    serverName = serverData['data']['attributes']['name']
+    serverStats = serverData['data']['attributes']['status']
+    serverMission = serverData['data']['attributes']['details']['mission']
+    activePlayer = serverData['data']['attributes']['players']
+    maxPlayer = serverData['data']['attributes']['maxPlayers']
+    
+    embed = discordEmbed(title=serverName, description=serverStats.title(), color=0x00ff00)
+    embed.set_thumbnail(url='https://units.arma3.com/groups/img/32641/06EvpC7yf0.png')
+    
+    if serverStats == 'online':
+        embed.add_field(name="Mission", value=serverMission, inline=True)
+        embed.add_field(name="Players", value=f'{activePlayer}/{maxPlayer}', inline=True)
+        embed.add_field(name="Active Players", value=players, inline=False)
+    
+    elif serverStats == 'dead' or serverStats == 'removed':
+        capture_message(f'{serverName} is {serverStats}')
+
+    return embed
+
+def retrieveData(db, option, title):
+    data_ref = db.collection(option).document(title)
+    docs = data_ref.get()
+    return docs.to_dict()   
+
+async def checkDb(db, serverlistDb, ctx, firestore):
+    serverList = retrieveData(db, option='serverlist', title=ctx.message.channel.id)
+    if serverList is None:
+        data = {'create':'create'}
+        serverlistDb.set(data)
+        serverlistDb.update({'create':firestore.DELETE_FIELD})
+        return
 
 load_dotenv(find_dotenv())
